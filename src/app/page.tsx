626c7f50f2085200ca6a63fb365533e25b8a3a66
Anupam Yadav <anupamy571@gmail.com>
2024-12-08 21:54:11 +0530

    More Sections Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import HeroSection from "@/components/HeroSection";
 import Projects from "@/components/Projects";
+import SocialLinks from "@/components/SocialLinks";
+import TestimonialCard from "@/components/TestimonialCard";
 import WhyMe from "@/components/WhyMe";
 
 export default function Home() {
   return (
-    <main className="border border-white">
+    <main>
     <HeroSection  />
     <Projects/>
     <WhyMe/>
+    <TestimonialCard/>
+    <SocialLinks/>
     </main>
   );
 }
